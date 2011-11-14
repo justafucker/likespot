306cd6d7654a100c9978dab0ef29dc4264fcd07e
Andrey Cheptsov <andrey.cheptsov@gmail.com>
2011-11-14 22:52:00 +0400

    Test 14

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -9,9 +9,9 @@ public class Bootstrap extends Job {
 
     public void doJob() {
         // Check if the database is empty
-        if(Product.count() == 0) {
+        /*if(Product.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
-        }
+        }*/
     }
 
 }
