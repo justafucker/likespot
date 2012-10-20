fe242a0c500b033cdbf6067270f677bd5c134403
Andrey Cheptsov <andrey.cheptsov@gmail.com>
2012-10-20 21:45:29 +0400

    Check is admin role and settings menu

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Category;
 import models.User;
+import play.mvc.Before;
 import play.mvc.With;
 
 import javax.persistence.OneToMany;
@@ -10,4 +11,11 @@ import java.util.List;
 @CRUD.For(User.class)
 @With(Secure.class)
 public class Users extends CRUD {
+    @Before
+    public static void checkUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user);
+        }
+    }
 }
